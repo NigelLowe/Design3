431096f861f5807686923da9d0c300081fd31af7
Nigel Lowe <nlow3178@uni.sydney.edu.au>
2020-03-26 19:27:32 +1100

    removed spaces

--- climb_cruise_iteration.m
+++ climb_cruise_iteration.m
@@ -1,30 +1,17 @@
 %Enviromental
-
 g    = 9.81;
-
 T0   = 288;
-
 L_r  = 0.0065; %lapse rate for temp
-
 R    = 287;
-
 rho0 = 1.225;
-
 P0   = 101300;
 
-
 %Initalise variables
-
 TOW     = endurance_target/2 * 1e3; %kg (guess starting weight)
-
 TOW_new = 2*TOW;
-
 n_time_pts = 1e4; 
-
 delta_counter = 0; %to avoid infinte loop
-
 time_res   = endurance_target/n_time_pts; %hrs
-
 
 % Mission schedule (in hrs)
 t_loiter_start = (loiter_point/v_cruise)/3600;
@@ -35,60 +22,39 @@ while abs(TOW_new - TOW)/TOW > 1e-5
     delta_counter = delta_counter + 1; 
 
     %Set inital conditions (starting conditions)
-
     w          = TOW;
-
     w_initial  = TOW;
-
     t          = 0; % set time to 0
-
     alt        = 0; % set altitude to 0
-
     counter_2  = 0; %counter for sake of PL drop
-
     PL_dropped = 0;
 
     while t <= endurance_target
 
         %determine ambients (Linear - No account for Tropo)
-
         temp = T0-alt*L_r;
-
         press= P0*(temp/T0)^(g/L_r/R);
-
         rho  = press/(R*temp);
-
         if t >= endurance_target/2  && counter_2 == 0 
-
             PL_dropped = PL + 500;
-
             w = w - PL_dropped; %loose PL
-
             cdo  = cd0c;  % drag improvement from external payload
-
             counter_2 = counter_2 + 1;
-
         end 
 
         L    = w*g; %N - Small angle assumption for climb
 
         % determine if aircraft in climb or cruise phase
-
         if alt < cruise_alt
 
             cl = cl_climb;
-
+            
             %Climb phase - Climb at min l_d
-
             D = L/ld_climb;
-
             v = (L/(0.5*cl*rho*S))^0.5; %m/s
-
            
             %Determine Thrust req. (from excess power eqn.)
-
             Thrust = target_roc*w*g/v+D;
-
             alt = alt + (Thrust - D)*v/L*time_res*3600;  %m, time res is in hrs
       
         else %Cruise or Loiter (aircraft needs to be at cruising level)
@@ -96,179 +62,108 @@ while abs(TOW_new - TOW)/TOW > 1e-5
            if t < t_loiter_start || t > t_loiter_end  %cruise out or back
 
             cl= L/(0.5*rho*S*v_cruise^2);
-
             cd   = cdo + k*(cl-clmin)^2;
-
             Thrust = cd*(0.5*rho*S*v_cruise^2);
             
            else
                
             cl= L/(0.5*rho*S*v_loiter^2);
-
             cd   = cdo + k*(cl-clmin)^2;
-
             Thrust = cd*(0.5*rho*S*v_loiter^2);
                   
            end
            
         end
 
-    
-
         %Fuel
-
         %(ff = Thrust*TSFC; % fuel flow)
-
         %(fused(i) = Thrust*TSFC*time_res*3600; %kg, as time res is in hrs)
 
-
-
         w = w - Thrust*TSFC*time_res*3600; %end of segement fuel
-
-            
-
         t = t + time_res; 
 
     end
 
-    
-
     % weights
-
     empty_weight = 1.66*TOW^0.815; %As per texts
-
     %empty_weight = 6000;            % Used when BEW is fixed
 
-    
-
     % Calc new TOW with mass fraction approach.
-
     TOW_new = (empty_weight + (w_initial - w) + (PL - PL_dropped))/(1-500/TOW);
 
-    
-
-    
-
     % Take mean of 2 values for stability
-
     TOW = (TOW + TOW_new)/2;
-
-    
 
     cdo = cd0; % reset cdo foor next loop
 
     if delta_counter > 500 % stop computing if TOW gets too big
-
         TOW = Inf;
-
         break;
-
     end
-
 end
 
-    %Set inital conditions (starting conditions)
+%Set inital conditions (starting conditions)
+w(1)       = TOW;
+t(1)       = 0; % set time to 0
+alt(1)     = 0; % set altitude to 0
+counter_2  = 0; %counter for sake of PL drop
+PL_dropped = 0;
+i = 1;
+while t(i) <= endurance_target
 
-    w(1)       = TOW;
+    %determine ambients (Linear - No account for Tropo)
+    temp = T0-alt(i)*L_r;
+    press= P0*(temp/T0)^(g/L_r/R);
+    rho  = press/(R*temp);
+    if t(i) >= endurance_target/2  && counter_2 == 0 
+        PL_dropped = PL + 500;
+        w(i) = w(i) - PL_dropped; %loose PL
+        cdo  = cd0c;  % drag improvement from external payload
+        counter_2 = counter_2 + 1;
+    end 
 
-    t(1)       = 0; % set time to 0
+    L(i)    = w(i)*g; %N - Small angle assumption for climb
 
-    alt(1)     = 0; % set altitude to 0
+    % determine if aircraft in climb or cruise phase
 
-    counter_2  = 0; %counter for sake of PL drop
+    if alt < cruise_alt
 
-    PL_dropped = 0;
-    
-    i = 1;
-
-    while t(i) <= endurance_target
-
-        %determine ambients (Linear - No account for Tropo)
-
-        temp = T0-alt(i)*L_r;
-
-        press= P0*(temp/T0)^(g/L_r/R);
-
-        rho  = press/(R*temp);
-
-        if t(i) >= endurance_target/2  && counter_2 == 0 
-
-            PL_dropped = PL + 500;
-
-            w(i) = w(i) - PL_dropped; %loose PL
-
-            cdo  = cd0c;  % drag improvement from external payload
-
-            counter_2 = counter_2 + 1;
-
-        end 
-
-        L(i)    = w(i)*g; %N - Small angle assumption for climb
-
-        % determine if aircraft in climb or cruise phase
-
-        if alt < cruise_alt
-
-            cl(i) = cl_climb;
-
-            %Climb phase - Climb at min l_d
-
-            D(i) = L(i)/ld_climb;
-
-            v(i) = (L(i)/(0.5*cl(i)*rho*S))^0.5; %m/s
-
-           
-            %Determine Thrust req. (from excess power eqn.)
-
-            Thrust(i) = target_roc*w(i)*g/v(i)+D(i);
-
-            alt(i+1) = alt(i) + (Thrust(i) - D(i))*v(i)/L(i)*time_res*3600;  %m, time res is in hrs
-      
-        else %Cruise or Loiter (aircraft needs to be at cruising level)
-            
-           if t(i) < t_loiter_start || t(i) > t_loiter_end  %cruise out or back
-
-            cl(i) = L(i)/(0.5*rho*S*v_cruise^2);
-
-            cd(i)   = cdo + k*(cl(i)-clmin)^2;
-
-            Thrust(i) = cd(i)*(0.5*rho*S*v_cruise^2);
-            
-            v(i) = v_cruise;
-            
-            alt(i+1) = alt(i); 
-            
-           else
-               
-            cl(i)= L(i)/(0.5*rho*S*v_loiter^2);
-
-            cd(i)   = cdo + k*(cl(i)-clmin)^2;
-
-            Thrust(i) = cd(i)*(0.5*rho*S*v_loiter^2);
-            
-            v(i) = v_loiter;
-            
-            alt(i+1) = alt(i);
-            
-           end
-           
-        end
-
-    
-
-        %Fuel
-
-        %(ff = Thrust*TSFC; % fuel flow)
-
-        %(fused(i) = Thrust*TSFC*time_res*3600; %kg, as time res is in hrs)
-
-
-
-        w(i+1) = w(i) - Thrust(i)*TSFC*time_res*3600; %end of segement fuel
-
-        t(i+1) = t(i) + time_res;
+        cl(i) = cl_climb;
         
-        %advance referencing parameter
-        i = i + 1; 
-        
+        %Climb phase - Climb at min l_d
+        D(i) = L(i)/ld_climb;
+        v(i) = (L(i)/(0.5*cl(i)*rho*S))^0.5; %m/s
+
+        %Determine Thrust req. (from excess power eqn.)
+        Thrust(i) = target_roc*w(i)*g/v(i)+D(i);
+        alt(i+1) = alt(i) + (Thrust(i) - D(i))*v(i)/L(i)*time_res*3600;  %m, time res is in hrs
+
+    else %Cruise or Loiter (aircraft needs to be at cruising level)
+
+       if t(i) < t_loiter_start || t(i) > t_loiter_end  %cruise out or back
+
+        cl(i) = L(i)/(0.5*rho*S*v_cruise^2);
+        cd(i)   = cdo + k*(cl(i)-clmin)^2;
+        Thrust(i) = cd(i)*(0.5*rho*S*v_cruise^2);
+        v(i) = v_cruise;
+        alt(i+1) = alt(i); 
+
+       else
+
+        cl(i)= L(i)/(0.5*rho*S*v_loiter^2);
+        cd(i)   = cdo + k*(cl(i)-clmin)^2;
+        Thrust(i) = cd(i)*(0.5*rho*S*v_loiter^2);
+        v(i) = v_loiter;
+        alt(i+1) = alt(i);
+
+       end
     end
+
+    w(i+1) = w(i) - Thrust(i)*TSFC*time_res*3600; %end of segement fuel
+    t(i+1) = t(i) + time_res;
+
+    %advance referencing parameter
+    i = i + 1; 
+
+end
+cdo = cd0; % reset for displaying on plot
--- prelim_report_code.m
+++ prelim_report_code.m
@@ -1,184 +1,101 @@
 clear all, 
-
 close all,
-
 clc
-
-
 
 % DESIGN 3 - Preliminary Sizing - Group 3
 
-
-
 %Wing + Aero Parameters
-
 S   = 68;        %m2
-
-AR  = 18;            
-
+AR  = 18;
 b   = sqrt(S*AR); %m
-
 c   = S/b;
 
-
-
 e   = 0.85;
-
 cdo = 0.03;
-
 k   = 1/(pi*e*AR);
 
-
-
 %Engine
-
 TSFC = 13*1e-6;   %kg/(s.N)
 
-
-
 %mission parameters
-
 reach_toc        = 6;  %hrs
-
 cruise_alt       = 40000/3.281; %m
-
 loiter_point    = 4000e3; %4000 km
-
 v_cruise        = convvel(250,'kts','m/s');
 v_loiter        = convvel(200,'kts','m/s');
 
 target_roc       = cruise_alt/reach_toc/3600; %m/s
-
 ld_climb         = 1/(2*sqrt(k*cdo)); %min l_d
-
 cl_climb         = sqrt(pi*AR*cdo*e);
-
-
 
 reserves         = 0.01; %decent/approach/taxi
 
-
-
 clmax = 1.8;
-
 clmin = 0.2; 
-
 cd0 = cdo;% with payload drag coefficient
-
 cd0c = 0.025; % no external payload drag coefficient
 
-
-
 %Mission Req
-
 % requirement 1 MAX PL and 40 hrs En
-
 mission_1_x = [40 40];
-
 mission_1_y = [0 60e3];
 
-
-
 % requirement 2 half PL and 50 hrs En
-
 mission_2_x = [50 50];
-
 mission_2_y = [0 60e3];
 
-
 % requirement 3 rotor takeoff
-
 mission_3_x = [40 55];
-
 mission_3_y = [23e3 23e3];
 
-
-
 %Vectors of PL and Endurance
-
 pl_vector = linspace(500,3500,4);
-
 en_vector = linspace(40,55,10); 
 
 %Wait bar for sanity
-
 hwait = waitbar(0,'Please wait...');
-
 h_bar = 1;
-
 for m_index = 1:length(pl_vector)
-
-    
 
     PL = pl_vector(m_index);
 
     for n_index = 1:length(en_vector)
 
-        
-
         endurance_target = en_vector(n_index);
-
-        
 
         climb_cruise_iteration
 
-        
-
         TOW_results(m_index,n_index) = TOW;
-      
 
         % Wait bar
-
         hwait = waitbar(h_bar/(length(pl_vector) * length(en_vector)));
-
         h_bar = h_bar + 1;
 
     end
 
 end
 
-
-
 close(hwait)
 
 
-
 %% Plot lines of contant Payload
-
 fig = figure(1);
-
 hold on
-
 grid on
-
 ylabel('TOW (kg.)')
-
 xlabel('Endurance (hrs.)')
-
 set(gca,'FontSize',18)
 
-
-
 % Plot lines of constant PL
-
 for j = 1:size(TOW_results,1)
 
-    
-
     plot(en_vector,TOW_results(j,:),'linewidth',3)
-
     legend_labels{j} = sprintf('%.0f kg.', pl_vector(j));
-
-    
 
 end
 
-
-
 %assumptions box
-
 assumptions = sprintf('cdo: %.4f, TSFC: %.6f, e: %.2f, alt: %.0f ft, S: %.0f m^2, AR: %.0f, Cruise: %.0f kts , Loiter: %.0f kts ', cdo,TSFC,e,cruise_alt*3.281,S,AR,convvel(v_cruise,'m/s','kts'),convvel(v_loiter,'m/s','kts'));
-
 annotation('textbox',...
     [0.33 0.86 0.21 0.05],...
     'String',assumptions,...
@@ -186,20 +103,13 @@ annotation('textbox',...
     'FontSize',12,...
     'FitBoxToText','off');
 
-
-
 % Plot mission lines
-
 plot(mission_1_x,mission_1_y,'--','color','k','linewidth',3)
-
 plot(mission_2_x,mission_2_y,'--','color','k','linewidth',3)
-
 plot(mission_3_x,mission_3_y,'--','color','k','linewidth',3)
 
 tow = TOW_results(~isinf(TOW_results));
-
 ylim([min(min(tow))-1e4,max(max(tow))+1e4])
-
 xlim([40,55])
 
 legend(legend_labels,'location','NW')
