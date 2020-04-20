36b97dedd0409d9f210faa6a238446c9fa54c4d5
Jack <62593276+Jack78000@users.noreply.github.com>
2020-04-20 13:38:38 +1000

    Update drag file with final calcs from CDR 1

--- dragEstimation.m
+++ dragEstimation.m
@@ -58,14 +58,28 @@ end
 
 fprintf('Skin Friction Coefficient, Cf = %.4f\n', Cf)
 
-% Choose the wing area as the reference area
-Sref = S;
+
 
 %% Get the wetted area for all area perpendicular to flow
+
+% Which config?
+config = 1;
 t = 0.152*c;    % Max thickness of airfoil
 L_D = 15;       % Best lift to drag ratio
 
+% Choose the wing area as the reference area
+Sref = c*b/1.45;
+
 Mach = v_mean/a;    % Mach number during cruise
+
+% Config chech
+if config == 0
+    no_pods = 0;
+    no_rotor = 0;
+else
+    no_pods = 4;
+    no_rotor = 2;
+end
 
 % Force terms
 f_tc = 1 + 2.7*(t/c) + 100*(t/c)^4;
@@ -75,34 +89,71 @@ f_m = 1 - 0.08*Mach^1.45;
 L_fuse = 16;
 D_fuse = 2;
 f_ld = 1 + 60/((L_fuse/D_fuse)^2) + 0.0025*(L_fuse/D_fuse);
-S_fuse = pi * (D_fuse/2)^2;
+%S_fuse = pi * (D_fuse/2)^2;
+S_fuse = pi*D_fuse*L_fuse*(0.5+0.135*(4/L_fuse))^2/3*(1.015+0.3/(L_fuse/D_fuse)^1.5);
 Cdo_fuse = Cf * f_ld * f_m * S_fuse/Sref;
-fprintf('\nFuselage: Swet = %.4f m^2, Cdo = %.5f\n', S_fuse, Cdo_fuse)
+fprintf('\nFuselage: Swet = %6.3f m^2, Cdo = %.5f\n', S_fuse, Cdo_fuse)
 
 % Wing 
-S_wing = b*t;
+lambda = 1.2/2.7; %Taper Ratio
+%S_wing = b*c;
+S_wing = 2*b*c*(1+0.25*(t/c)*((1+t/c*lambda)/(1+lambda)));
 Cdmin_airfoil = 0.01;
 Cdo_wing = Cf * f_tc * f_m * S_wing/Sref * (Cdmin_airfoil/0.004)^0.4;
-fprintf('Wing:     Swet = %.4f m^2, Cdo = %.5f\n', S_wing, Cdo_wing)
+fprintf('Wing:     Swet = %6.3f m^2, Cdo = %.5f\n', S_wing, Cdo_wing)
 
 % Tailplane
 b_tail = 4.6;
-t_tail = 0.152 * 0.5*(2.5+1.1);
-S_tail = b_tail * t_tail;
+c_tail = 0.5*(2.5+1.1);
+t_tail = 0.152 * c_tail;
+lambda_tail = 1.1/2.5;
+%S_tail = b_tail * c_tail;
+S_tail = 2*b_tail*c_tail*(1+0.25*(t/c)*((1+t/c*lambda_tail)/(1+lambda_tail)));
 Cdo_tail = Cf * f_tc * f_m * S_tail/Sref * (Cdmin_airfoil/0.004)^0.4;
-fprintf('Tail:     Swet = %.4f m^2, Cdo = %.5f\n', S_tail, Cdo_tail)
+fprintf('Tail:     Swet = %6.3f m^2, Cdo = %.5f\n', S_tail, Cdo_tail)
 
 % Rotor
-
+b_rotor = 6.75*2;
+c_rotor = 0.7;
+t_rotor = 0.12 * c_rotor;
+%S_rotor = b_rotor * c_rotor;
+S_rotor = 2*b_rotor*c_rotor*(1+0.25*(t_rotor/c_rotor)*((1+t_rotor/c_rotor*1)/(1+1)));
+Q_rotor = 1.2;
+Cdo_rotor = no_rotor * Q_rotor * (Cf * f_tc * f_m * S_rotor/Sref * (0.02/0.004)^0.4);
+fprintf('Rotor:    Swet = %6.3f m^2, Cdo = %.5f\n', S_rotor, Cdo_rotor)
 
 % Payload - External Pods
-no_pods = 4;
-D_pod = 0.18;
+D_pod = 0.4;
 L_pod = 1.8;
-S_pod = pi * (D_pod/2)^2;
+%S_pod = pi * (D_pod/2)^2;
+S_pod = pi*D_pod*L_pod*(0.5+0.135*(4/L_pod))^2/3*(1.015+0.3/(L_pod/D_pod)^1.5);
 f_ld_pod = 1 + 60/((L_pod/D_pod)^2) + 0.0025*(L_pod/D_pod);
-Cdo_pod = Cf * f_ld_pod * f_m * S_pod/Sref;
-fprintf('Pod:      Swet = %.4f m^2, Cdo = %.5f\n', S_fuse, no_pods*Cdo_pod)
+Q_pod = 1.5;
+Cdo_pod = Cf * Q_pod * f_ld_pod * f_m * S_pod/Sref;
+fprintf('Pod:      Swet = %6.3f m^2, Cdo = %.5f\n', S_pod, Cdo_pod)
+
+fprintf('\nAirfield Config:')
+% Total skin friction Drag
+Cdf = 1 * (Cdo_fuse + Cdo_wing + Cdo_tail + no_pods*Cdo_pod);
+fprintf('\nSkin Friction Drag coef, Cf = %.4f\n', Cdf)
+
+%Cdf = Cf * Swet / Sref;
+Cdo = 1.2 * Cdf;    % Extra 20% Accounts for pressure drag
+Cdo = 1.05 * Cdo;   % Extra 5% Accounts for Interference effects
+fprintf('Min Drag Coef, Cdo = %.4f\n', Cdo)
+
+%% Maritime config
+fprintf('\nMaritime Config:')
+% Total skin friction Drag
+Cdf = 1 * (Cdo_fuse + Cdo_wing + Cdo_tail + Cdo_rotor + no_pods*Cdo_pod);
+fprintf('\nSkin Friction Drag coef, Cf = %.4f\n', Cdf)
+
+%Cdf = Cf * Swet / Sref;
+Cdo = 1.2 * Cdf;    % Extra 20% Accounts for pressure drag
+Cdo = 1.05 * Cdo;   % Extra 5% Accounts for Interference effects
+fprintf('Min Drag Coef, Cdo = %.4f\n', Cdo)
+
+%% Angus equation
 
 %Interference Factor
 Q_nacelle = 1.5*3;
@@ -112,10 +163,11 @@ Q_fuselage = 1;
 
 Q_total = Q_nacelle+Q_wing+Q_fuselage+Q_fuselage;
 
-% Total skin friction Drag
-Cdf = Q_total * (Cdo_fuse + Cdo_wing + Cdo_tail + no_pods*Cdo_pod);
-fprintf('\nSkin Friction Drag coef, Cf = %.4f\n', Cdf)
+% Form Factor
+xi = 0.4; %position of max thickness
+sweep = 1; % No sweep, cos(x) = 1
+FF = (1+(0.6/xi)*(t/c)+100*(t/c)^4)*(1.34*Mach^0.18*(sweep)^0.28);
 
-%Cdf = Cf * Swet / Sref;
-Cdo = 1.2 * Cdf;
-fprintf('Min Drag Coef, Cdo = %.4f\n', Cdo)
+Swet_total = S_fuse + S_wing + S_tail + no_pods*S_pod;
+CD0_new = Cf * FF * Q_total * (Swet_total/Sref);
+%fprintf('Angus Drag: Cdo = %.4f\n', CD0_new)
